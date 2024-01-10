bf1864e09106e824b33da677ee1ce50051a0ec6c
Veronica Ni <vni1294@gmail.com>
2024-01-09 20:42:49 -0500

    Imported fetchMock, figuring out where to include it in test valid for POST /

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,19 +1,24 @@
 "use strict";
 
+const fetchMock = require("fetch-mock");
 const request = require("supertest");
 const app = require("../app");
 
 
 describe("POST /", function () {
   test("valid", async function () {
-    const resp = await request(app).post("/shipments").send({
+    const resp = await request(app)
+    .post("/shipments")
+    .send({
       productId: 1000,
       name: "Test Tester",
       addr: "100 Test St",
       zip: "12345-6789",
     });
 
+    //mock Response from shipItAPI : 3
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    //API {shipped: 3}
   });
 
   test("throws error if empty request body", async function () {
